ec05e8986f61f71ba41b46e933cb596ad566bf5c
Nicholas_Huang713 <nhuang713@gmail.com>
2020-03-01 10:23:21 -0800

    Updates

---
+++ precache-manifest.c30f97a1afd1032308ecb808d5d06af3.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e5f42613d7c352f619bfb65f49bbada",
+    "url": "/musicapp-deploy/index.html"
+  },
+  {
+    "revision": "f7475104c36a62170082",
+    "url": "/musicapp-deploy/static/css/main.0dc901a6.chunk.css"
+  },
+  {
+    "revision": "dc9b25d7269673ec0981",
+    "url": "/musicapp-deploy/static/js/2.20ac9021.chunk.js"
+  },
+  {
+    "revision": "f7475104c36a62170082",
+    "url": "/musicapp-deploy/static/js/main.42b4a93c.chunk.js"
+  },
+  {
+    "revision": "25e9e1be04867155dbc1",
+    "url": "/musicapp-deploy/static/js/runtime-main.fd889b18.js"
+  },
+  {
+    "revision": "7d66f8285694f8c175a744518cfa8692",
+    "url": "/musicapp-deploy/static/media/70sllcool.7d66f828.jpg"
+  },
+  {
+    "revision": "b06b4c0ea14a91979f3994f1a582d7d1",
+    "url": "/musicapp-deploy/static/media/80s.b06b4c0e.jpg"
+  },
+  {
+    "revision": "fe4da0a2dbd4c3ede185d2ce26b49bea",
+    "url": "/musicapp-deploy/static/media/90s.fe4da0a2.jpg"
+  },
+  {
+    "revision": "38a769786a0bf7829de9b2c547892105",
+    "url": "/musicapp-deploy/static/media/audience.38a76978.jpg"
+  },
+  {
+    "revision": "b65a12c34ee42ec34a63aea148d7dcc9",
+    "url": "/musicapp-deploy/static/media/djblue.b65a12c3.jpg"
+  },
+  {
+    "revision": "48908e8e5ed7d9ee2c885bd076396639",
+    "url": "/musicapp-deploy/static/media/hiphopcrowd1.48908e8e.jpg"
+  },
+  {
+    "revision": "cbd99399a37bbf1678a4824f202303b4",
+    "url": "/musicapp-deploy/static/media/liked.cbd99399.png"
+  },
+  {
+    "revision": "0b8c8962afeaf3d80291415bd527a90e",
+    "url": "/musicapp-deploy/static/media/mic.0b8c8962.jpg"
+  },
+  {
+    "revision": "a72f52e24991e11aa9768f458c02c8ba",
+    "url": "/musicapp-deploy/static/media/records.a72f52e2.png"
+  },
+  {
+    "revision": "ad6c7d245cf0d0759984c588e796b19e",
+    "url": "/musicapp-deploy/static/media/turntable.ad6c7d24.png"
+  },
+  {
+    "revision": "c2fa355eb07f71c1c114a3bce5931af7",
+    "url": "/musicapp-deploy/static/media/turntable2.c2fa355e.jpg"
+  },
+  {
+    "revision": "6a2d0c9f0f105ca1cd45282bb136267a",
+    "url": "/musicapp-deploy/static/media/turntable3.6a2d0c9f.jpg"
+  },
+  {
+    "revision": "ebdb60f094dc35b9248f23d364becfd2",
+    "url": "/musicapp-deploy/static/media/unliked.ebdb60f0.png"
+  }
+]);
